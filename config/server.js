0d8edffda520b98edd13e08d593e58248e1b925a
Felipe Marinho <felipmarinho@gmail.com>
2020-11-15 16:09:24 -0300

    Comite inicial do projeto Strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4f21cd911a19adff230fa6ad96ddb9b6'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b7d4adae-4630-4397-8a65-d0c1e8373b22'
+};
